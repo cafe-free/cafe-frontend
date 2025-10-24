56e785bfcc3c867893f4ba3f946782d734c3a327
Brian Lee <cy12270@gmail.com>
2025-10-24 17:07:16 +0800

    change menu data and add images

--- src/assets/js/data.js
+++ src/assets/js/data.js
@@ -1,35 +1,326 @@
-const defaultImg = 'src/assets/images/PickUp01.png';
 
 export const menuData = [
-  // Food
-  { category: "Food", subcategory: "Pizza", title: "Margherita Pizza", price: 88, img: defaultImg },
-  { category: "Food", subcategory: "Pizza", title: "Pepperoni Pizza", price: 92, img: defaultImg },
-  { category: "Food", subcategory: "Pasta", title: "Spaghetti Carbonara", price: 95, img: defaultImg },
-  { category: "Food", subcategory: "Pasta", title: "Penne Arrabiata", price: 90, img: defaultImg },
-  { category: "Food", subcategory: "Salad", title: "Caesar Salad", price: 72, img: defaultImg },
-  { category: "Food", subcategory: "Salad", title: "Greek Salad", price: 70, img: defaultImg },
-  { category: "Food", subcategory: "Sandwich", title: "Grilled Chicken Sandwich", price: 80, img: defaultImg },
-  { category: "Food", subcategory: "Sandwich", title: "Tuna Melt Sandwich", price: 78, img: defaultImg },
-  { category: "Food", subcategory: "Burger", title: "Beef Burger", price: 98, img: defaultImg },
-  { category: "Food", subcategory: "Burger", title: "Veggie Burger", price: 85, img: defaultImg },
+  {
+    category: 'Food',
+    subcategory: 'bread',
+    title: 'Bread Oil',
+    price: 7.2,
+    img: './src/assets/images/menu/bread/Bread_BreadOil.png'
+  },
+  {
+    category: 'Food',
+    subcategory: 'bread',
+    title: 'Calzones',
+    price: 4.5,
+    img: './src/assets/images/menu/bread/Bread_Calzones.png'
+  },
+  {
+    category: 'Food',
+    subcategory: 'bread',
+    title: 'Cinnamon Roll',
+    price: 6.9,
+    img: './src/assets/images/menu/bread/Bread_CinnamonRoll.png'
+  },
+  {
+    category: 'Food',
+    subcategory: 'bread',
+    title: 'Pumpkin Bread',
+    price: 2.8,
+    img: './src/assets/images/menu/bread/Bread_PumpkinBread.png'
+  },
+  {
+    category: 'Food',
+    subcategory: 'bread',
+    title: 'Monkey Bread',
+    price: 7.2,
+    img: './src/assets/images/menu/bread/Bread_MonkeyBread.png'
+  },
+  {
+    category: 'Food',
+    subcategory: 'bread',
+    title: 'Fruit Bread',
+    price: 4.5,
+    img: './src/assets/images/menu/bread/Bread_FruitBread.png'
+  },
+  {
+    category: 'Food',
+    subcategory: 'bread',
+    title: 'Fougasse',
+    price: 6.9,
+    img: './src/assets/images/menu/bread/Bread_Fougasse.png'
+  },
+  {
+    category: 'Food',
+    subcategory: 'sandwich',
+    title: 'Ham Sandwich',
+    price: 7.2,
+    img: './src/assets/images/menu/bread/Bread_HamSandwich.png'
+  },
+  {
+    category: 'Food',
+    subcategory: 'sandwich',
+    title: 'Turkey Club Sandwich',
+    price: 4.5,
+    img: './src/assets/images/menu/bread/Bread_TurkeyClubSandwich.png'
+  },
+  {
+    category: 'Food',
+    subcategory: 'sandwich',
+    title: 'Burritos',
+    price: 6.9,
+    img: './src/assets/images/menu/bread/Bread_Burritos.png'
+  },
+  {
+    category: 'Food',
+    subcategory: 'sandwich',
+    title: 'Sandwich',
+    price: 2.8,
+    img: './src/assets/images/menu/bread/Bread_Sandwich.png'
+  },
+  {
+    category: 'Food',
+    subcategory: 'cookie',
+    title: 'Cinnamon Cookies',
+    price: 7.2,
+    img: './src/assets/images/menu/Cookie/Cookie_CinnamonCookies.png'
+  },
+  {
+    category: 'Food',
+    subcategory: 'cookie',
+    title: 'Chocolate Chip Cookies',
+    price: 4.5,
+    img: './src/assets/images/menu/Cookie/Cookie_ChocolateChipCookies.png'
+  },
+  {
+    category: 'Food',
+    subcategory: 'cookie',
+    title: 'Thumbprint',
+    price: 6.9,
+    img: './src/assets/images/menu/Cookie/Cookie_Thumbprint.png'
+  },
+  {
+    category: 'Food',
+    subcategory: 'cookie',
+    title: 'Peanut Butter Cookie Cups',
+    price: 2.8,
+    img: './src/assets/images/menu/Cookie/Cookie_PeanutButterCookieCups.png'
+  },
+  {
+    category: 'Food',
+    subcategory: 'cake',
+    title: 'Almond Frangipane',
+    price: 7.2,
+    img: './src/assets/images/menu/Cake/Cake_AlmondFrangipane.png'
+  },
+  {
+    category: 'Food',
+    subcategory: 'cake',
+    title: 'Black Tea Cake',
+    price: 4.5,
+    img: './src/assets/images/menu/Cake/Cake_BlackTeaCake.png'
+  },
+  {
+    category: 'Food',
+    subcategory: 'cake',
+    title: 'Chocolate Cup Cakes',
+    price: 6.9,
+    img: './src/assets/images/menu/Cake/Cake_ChocolateCupCakes.png'
+  },
+  {
+    category: 'Food',
+    subcategory: 'cake',
+    title: 'Matcha White Chocolate',
+    price: 2.8,
+    img: './src/assets/images/menu/Cake/Cake_MatchWhiteChocolate.png'
+  },
+  {
+    category: 'Food',
+    subcategory: 'cake',
+    title: 'Red Velvet Royale',
+    price: 7.2,
+    img: './src/assets/images/menu/Cake/Cake_RedVekvetRoyale.png'
+  },
+  {
+    category: 'Food',
+    subcategory: 'cake',
+    title: 'Banana Walnut Loaf',
+    price: 4.5,
+    img: './src/assets/images/menu/Cake/Cake_BananaWalnutLoaf.png'
+  },
+  {
+    category: 'Food',
+    subcategory: 'cake',
+    title: 'Chocolate Cheesecake',
+    price: 6.9,
+    img: './src/assets/images/menu/Cake/Cake_ChocolateCheesecake.png'
+  },
+  {
+    category: 'Food',
+    subcategory: 'cake',
+    title: 'Lemon Drizzle Cake',
+    price: 2.8,
+    img: './src/assets/images/menu/Cake/Cake_LemonDrizzleCake.png'
+  },
+  {
+    category: 'Food',
+    subcategory: 'cake',
+    title: 'Opera Slice',
+    price: 6.9,
+    img: './src/assets/images/menu/Cake/Cake_OperaSlice.png'
+  },
+  {
+    category: 'Food',
+    subcategory: 'cake',
+    title: 'Tiramisu Delight',
+    price: 2.8,
+    img: './src/assets/images/menu/Cake/Cake_TiramisuDelight.png'
+  },
+  {
+    category: 'Drinks',
+    subcategory: 'coffee',
+    title: 'Americano',
+    price: 3.8,
+    img: './src/assets/images/menu/coffee/coffee_Americano.png'
+  },
+  {
+    category: 'Drinks',
+    subcategory: 'coffee',
+    title: 'Cappuccino',
+    price: 3.8,
+    img: './src/assets/images/menu/coffee/coffee_Cappuccino.png'
+  },
+  {
+    category: 'Drinks',
+    subcategory: 'coffee',
+    title: 'Cold Brew',
+    price: 3,
+    img: './src/assets/images/menu/coffee/coffee_Coldbrew.png'
+  },
+  {
+    category: 'Drinks',
+    subcategory: 'coffee',
+    title: 'Expresso',
+    price: 2.5,
+    img: './src/assets/images/menu/coffee/coffee_Expresso.png'
+  },
+  {
+    category: 'Drinks',
+    subcategory: 'coffee',
+    title: 'Flatwhite',
+    price: 2.5,
+    img: './src/assets/images/menu/coffee/coffee_Flatwhite.png'
+  },
+  {
+    category: 'Drinks',
+    subcategory: 'coffee',
+    title: 'Frappe',
+    price: 2.5,
+    img: './src/assets/images/menu/coffee/coffee_Frappe.png'
+  },
+  {
+    category: 'Drinks',
+    subcategory: 'coffee',
+    title: 'Irsh Coffee',
+    price: 2.5,
+    img: './src/assets/images/menu/coffee/coffee_Irshcoffee.png'
+  },
+  {
+    category: 'Drinks',
+    subcategory: 'coffee',
+    title: 'Latte',
+    price: 2.5,
+    img: './src/assets/images/menu/coffee/coffee_Latte.png'
+  },
+  {
+    category: 'Drinks',
+    subcategory: 'coffee',
+    title: 'Long Black',
+    price: 2.5,
+    img: './src/assets/images/menu/coffee/coffee_Longblack.png'
+  },
+  {
+    category: 'Drinks',
+    subcategory: 'coffee',
+    title: 'Mocha',
+    price: 2.5,
+    img: './src/assets/images/menu/coffee/coffee_Mocha.png'
+  },
+  {
+    category: 'Drinks',
+    subcategory: 'tea',
+    title: 'Black Tea',
+    price: 2.5,
+    img: './src/assets/images/menu/tea/tea_Blacktea.png'
+  },
+  {
+    category: 'Drinks',
+    subcategory: 'tea',
+    title: 'Ginger Tea',
+    price: 2.5,
+    img: './src/assets/images/menu/tea/tea_Gingertea.png'
+  },
+  {
+    category: 'Drinks',
+    subcategory: 'tea',
+    title: 'Green Tea',
+    price: 2.5,
+    img: './src/assets/images/menu/tea/tea_Greentea.png'
+  },
+  {
+    category: 'Drinks',
+    subcategory: 'tea',
+    title: 'White Tea',
+    price: 2.5,
+    img: './src/assets/images/menu/tea/tea_Whitetea.png'
+  },
+  {
+    category: 'Drinks',
+    subcategory: 'juice',
+    title: 'Beatroot',
+    price: 2.5,
+    img: './src/assets/images/menu/juice/juice_Beatroot.png'
+  },
+  {
+    category: 'Drinks',
+    subcategory: 'juice',
+    title: 'Kiwi',
+    price: 2.5,
+    img: './src/assets/images/menu/juice/juice_Kiwi.png'
+  },
+  {
+    category: 'Drinks',
+    subcategory: 'juice',
+    title: 'Lemonada',
+    price: 2.5,
+    img: './src/assets/images/menu/juice/juice_Lemonada.png'
+  },
+  {
+    category: 'Drinks',
+    subcategory: 'juice',
+    title: 'Mongo',
+    price: 2.5,
+    img: './src/assets/images/menu/juice/juice_Mongo.png'
+  },
+  {
+    category: 'Drinks',
+    subcategory: 'juice',
+    title: 'Orange',
+    price: 2.5,
+    img: './src/assets/images/menu/juice/juice_Orange.png'
+  },
+  {
+    category: 'Drinks',
+    subcategory: 'juice',
+    title: 'Tomato',
+    price: 2.5,
+    img: './src/assets/images/menu/juice/juice_Tomato.png'
+  },
+  {
+    category: 'Drinks',
+    subcategory: 'juice',
+    title: 'Watermelon',
+    price: 2.5,
+    img: './src/assets/images/menu/juice/juice_Watermelon.png'
+  }
+]
 
-  // Drinks
-  { category: "Drinks", subcategory: "Tea", title: "Iced Lemon Tea", price: 28, img: defaultImg },
-  { category: "Drinks", subcategory: "Tea", title: "Green Tea", price: 26, img: defaultImg },
-  { category: "Drinks", subcategory: "Coffee", title: "Cappuccino", price: 35, img: defaultImg },
-  { category: "Drinks", subcategory: "Coffee", title: "Latte", price: 36, img: defaultImg },
-  { category: "Drinks", subcategory: "Coffee", title: "Espresso", price: 30, img: defaultImg },
-  { category: "Drinks", subcategory: "Juice", title: "Fresh Orange Juice", price: 32, img: defaultImg },
-  { category: "Drinks", subcategory: "Juice", title: "Apple Juice", price: 30, img: defaultImg },
-  { category: "Drinks", subcategory: "Water", title: "Sparkling Water", price: 25, img: defaultImg },
-  { category: "Drinks", subcategory: "Water", title: "Still Mineral Water", price: 20, img: defaultImg },
-
-  // Dessert
-  { category: "Dessert", subcategory: "Cake", title: "Chocolate Lava Cake", price: 55, img: defaultImg },
-  { category: "Dessert", subcategory: "Cake", title: "Strawberry Cheesecake", price: 60, img: defaultImg },
-  { category: "Dessert", subcategory: "Cake", title: "Carrot Cake", price: 52, img: defaultImg },
-  { category: "Dessert", subcategory: "Ice Cream", title: "Vanilla Ice Cream", price: 38, img: defaultImg },
-  { category: "Dessert", subcategory: "Ice Cream", title: "Mint Chocolate Chip Ice Cream", price: 40, img: defaultImg },
-  { category: "Dessert", subcategory: "Pastry", title: "Croissant", price: 28, img: defaultImg },
-  { category: "Dessert", subcategory: "Pastry", title: "Apple Pie", price: 48, img: defaultImg }
-];
