270dd8c4b8cb609034bbbd6471150b8443c5c7bd
cy12270 <cy12270@gmail.com>
2025-11-04 16:58:25 +0800

    update route

--- app/api/menu/route.ts
+++ app/api/menu/route.ts
@@ -1,53 +1,13 @@
+import connectDB from '@/lib/db';
+import Menu from '@/models/Menu';
 import { NextResponse } from 'next/server'
 export const dynamic = 'force-static'
+// export const revalidate = 60; 
 
 export async function GET() {
     try {
-        const menuData = [
-        {
-            category: 'Food',
-            subcategory: 'bread',
-            title: 'Bread Oil',
-            price: 7.2,
-            img: '/menu/bread/Bread_BreadOil.png'
-        },
-        {
-            category: 'Food',
-            subcategory: 'bread',
-            title: 'Calzones',
-            price: 4.5,
-            img: '/menu/bread/Bread_Calzones.png'
-        },
-        {
-            category: 'Food',
-            subcategory: 'bread',
-            title: 'Cinnamon Roll',
-            price: 6.9,
-            img: '/menu/bread/Bread_CinnamonRoll.png'
-        },
-        {
-            category: 'Food',
-            subcategory: 'bread',
-            title: 'Pumpkin Bread',
-            price: 2.8,
-            img: '/menu/bread/Bread_PumpkinBread.png'
-        },
-        {
-            category: 'Food',
-            subcategory: 'bread',
-            title: 'Monkey Bread',
-            price: 7.2,
-            img: '/menu/bread/Bread_MonkeyBread.png'
-        },
-        {
-            category: 'Food',
-            subcategory: 'bread',
-            title: 'Fruit Bread',
-            price: 4.5,
-            img: '/menu/bread/Bread_FruitBread.png'
-        }
-        ];
-
+        await connectDB();
+        const menuData = await (Menu as any).find({}).lean();
         return NextResponse.json(menuData, {
             status: 200,
             headers: {
